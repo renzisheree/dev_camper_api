9d0fc5f2e9807ab4de7bb6b880599cafab205fca
Thanh Luong <mapneverdie1@gmail.com>
2024-07-15 01:03:07 +0700

    add geocoder questmap api

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 const bootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -95,4 +96,23 @@ bootcampSchema.pre("save", function (next) {
   next();
 });
 
+//create location field - geocode
+bootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // not saving address to DB
+
+  this.address = undefined;
+  next();
+});
 module.exports = mongoose.model("Bootcamp", bootcampSchema);
