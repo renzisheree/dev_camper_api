ca2a7b5d86b73339f45d8f9616ad1dc7b777bdc8
Thanh Luong <mapneverdie1@gmail.com>
2024-07-15 00:08:28 +0700

    slugify slug

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 const bootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -86,6 +87,12 @@ const bootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+//create bootcamp slug by slugify
+bootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", bootcampSchema);
