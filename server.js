c3dc490bc018385091d3b0c79b3599e9e1e8742c
Thanh Luong <mapneverdie1@gmail.com>
2024-07-17 03:05:33 +0700

    sync

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config({ path: "./config/config.env" });
 const path = require("path");
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileUpload = require("express-fileupload");
@@ -22,6 +23,7 @@ app.use(fileUpload());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
