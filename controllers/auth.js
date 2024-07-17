04b576608f8f3afa9595bffbbced7c91054837e1
Thanh Luong <mapneverdie1@gmail.com>
2024-07-18 02:02:59 +0700

    get self

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,21 +52,11 @@ const sendTokenResponse = (user, statusCode, res) => {
 };
 // @desc Get current logged user
 // @route POST /api/v1/auth/me
-// @access public
-
-exports.login = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
-  //validate
-  if (!email || !password) {
-    return next(new ErrorResponse("Please provide an email and password", 400));
-  }
-  const user = await User.findOne({ email }).select("+password");
-  if (!user) {
-    return next(new ErrorResponse("Invalid credentials", 401));
-  }
-  const isMatch = await user.matchPassword(password);
-  if (!isMatch) {
-    return next(new ErrorResponse("Invalid credentials", 401));
-  }
-  sendTokenResponse(user, 200, res);
+// @access Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
